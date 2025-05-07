36172119205ddbbf4d3b60ba9f93b5f84961fb5e
AbdallahDaradke <21110446@htu.edu.jo>
2025-05-07 14:38:36 +0300

    finishing-vite-and-starting-router

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+function App() {
+  return (
+    <Router>
+      <div>
+        <h1>React Router Demo</h1>
+
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
